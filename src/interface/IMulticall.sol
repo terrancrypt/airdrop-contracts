c3acde882fee359f1c2236b3c2a5adcaac12c193
terrancrypt <thaiphamngoctuong@gmail.com>
2024-04-22 13:12:21 +0700

    remove Multicall

--- src/interface/IMulticall.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-/**
- * @dev Provides a function to batch together multiple calls in a single external call.
- */
-interface IMulticall {
-    function multicall(bytes[] calldata data) external returns (bytes[] memory results);
-}
